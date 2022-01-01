7be3cb44b66a4338f2ad2dc9a146fec075958532
gurkangur <gurkangur154@gmail.com>
2022-01-01 16:26:35 +0300

    add campaign repository

---
+++ src/CampaignModule/Domain/AggregatesModel/CampaignAggregate/ICampaignRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace CampaignModule.Domain.AggregatesModel.CampaignAggregate
+{
+    public interface ICampaignRepository
+    {
+        Campaign Add(Campaign campaign);
+        Campaign GetByName(string name);
+        Campaign GetByProductCode(string productCode);
+        IEnumerable<Campaign> GetWhere(Expression<Func<Campaign, bool>> predicate);
+    }
+}
---
+++ src/CampaignModule/Infrastructure/Repositories/CampaignRepository.cs
@@ -0,0 +1,32 @@
+﻿using CampaignModule.Domain.AggregatesModel.CampaignAggregate;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace CampaignModule.Infrastructure.Repositories
+{
+    public class CampaignRepository : ICampaignRepository
+    {
+        private static List<Campaign> campaigns = new List<Campaign>();
+        public Campaign Add(Campaign campaign)
+        {
+            campaigns.Add(campaign);
+            return campaign;
+        }
+
+        public Campaign GetByName(string name)
+        {
+            return campaigns.FirstOrDefault(t => t.Name == name);
+        }
+        public Campaign GetByProductCode(string productCode)
+        {
+            return campaigns.FirstOrDefault(t => t.ProductCode == productCode);
+        }
+
+        public IEnumerable<Campaign> GetWhere(Expression<Func<Campaign, bool>> predicate)
+        {
+            return campaigns.Where(predicate.Compile()).ToList();
+        }
+    }
+}
