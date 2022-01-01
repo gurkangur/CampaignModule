1a57375446b3f42a758e32386b80c39777e4d07e
gurkangur <gurkangur154@gmail.com>
2022-01-01 16:47:41 +0300

    add campaign check to order

--- src/CampaignModule/Application/Orders/OrderService.cs
+++ src/CampaignModule/Application/Orders/OrderService.cs
@@ -1,4 +1,5 @@
-﻿using CampaignModule.Application.Products;
+﻿using CampaignModule.Application.Campaigns;
+using CampaignModule.Application.Products;
 using CampaignModule.Domain.AggregatesModel.OrderAggregate;
 
 namespace CampaignModule.Application.Orders
@@ -7,17 +8,24 @@ namespace CampaignModule.Application.Orders
     {
         private readonly IOrderRepository _orderRepository;
         private readonly IProductService _productService;
+        private readonly ICampaignService _campaignService;
 
-        public OrderService(IOrderRepository orderRepository, IProductService productService)
+        public OrderService(IOrderRepository orderRepository, IProductService productService, ICampaignService campaignService)
         {
             _orderRepository = orderRepository;
             _productService = productService;
+            _campaignService = campaignService;
         }
 
         public Order CreateOrder(Order order)
         {
             var product = _productService.GetByProductCode(order.ProductCode);
             _productService.DecreaseStock(order.ProductCode, order.Quantity);
+
+            var existingCampaign = _campaignService.GetByProductCode(order.ProductCode);
+            if (existingCampaign is not null)
+                _campaignService.IncraseSales(existingCampaign.Name, order.Quantity, product.Price);
+
             order.SetPrice(product.Price);
             return _orderRepository.Add(order);
         }
