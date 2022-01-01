e832d49a42bc2323d73719be166e31a56f28ea11
gurkangur <gurkangur154@gmail.com>
2022-01-01 16:10:04 +0300

    init

---
+++ src/CampaignModule/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CampaignModule
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
