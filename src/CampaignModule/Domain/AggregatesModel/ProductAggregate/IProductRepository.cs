23cced45ec335c0c1dad4382ea570e5df8928416
gurkangur <gurkangur154@gmail.com>
2022-01-01 16:18:38 +0300

    add product repository

---
+++ src/CampaignModule/Domain/AggregatesModel/ProductAggregate/IProductRepository.cs
@@ -0,0 +1,8 @@
+﻿namespace CampaignModule.Domain.AggregatesModel.ProductAggregate
+{
+    public interface IProductRepository
+    {
+        Product Add(Product product);
+        Product GetByProductCode(string productCode);
+    }
+}
---
+++ src/CampaignModule/Infrastructure/Repositories/ProductRepository.cs
@@ -0,0 +1,22 @@
+﻿using CampaignModule.Domain.AggregatesModel.ProductAggregate;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CampaignModule.Infrastructure.Repositories
+{
+    public class ProductRepository : IProductRepository
+    {
+        private static List<Product> products = new List<Product>();
+
+        public Product Add(Product product)
+        {
+            products.Add(product);
+            return product;
+        }
+
+        public Product GetByProductCode(string productCode)
+        {
+            return products.FirstOrDefault(t => t.Code == productCode);
+        }
+    }
+}
