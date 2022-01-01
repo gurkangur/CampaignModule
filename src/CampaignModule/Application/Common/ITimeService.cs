d24ab3ba1fba43465f19ece29e1ad206873d0dc3
gurkangur <gurkangur154@gmail.com>
2022-01-01 16:33:40 +0300

    add time service

---
+++ src/CampaignModule/Application/Common/ITimeService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace CampaignModule.Application.Common
+{
+    public interface ITimeService
+    {
+        TimeSpan Now { get; }
+        TimeSpan IncraseTime(int hour);
+    }
+}
---
+++ src/CampaignModule/Application/Common/TimeService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace CampaignModule.Application.Common
+{
+    public class TimeService : ITimeService
+    {
+        private static TimeSpan _now = new TimeSpan(0, 0, 0);
+
+        public TimeSpan Now => _now;
+
+        public TimeSpan IncraseTime(int hour)
+        {
+            _now = _now.Add(new TimeSpan(hour, 0, 0));
+            return _now;
+        }
+    }
+}
