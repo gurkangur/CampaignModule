c703375a3031bea39a04a30743ec526ab89a7b22
gurkangur <gurkangur154@gmail.com>
2022-01-01 16:24:05 +0300

    add campaign entity

---
+++ src/CampaignModule/Domain/AggregatesModel/CampaignAggregate/Campaign.cs
@@ -0,0 +1,42 @@
+﻿using CampaignModule.Domain.SeedWork;
+
+namespace CampaignModule.Domain.AggregatesModel.CampaignAggregate
+{
+    public class Campaign : Entity
+    {
+        public string Name { get; private set; }
+        public string ProductCode { get; private set; }
+        public int Duration { get; private set; }
+        public int PriceManipulationLimit { get; private set; }
+        public int TargetSalesCount { get; private set; }
+        public int TotalSalesCount { get; private set; }
+        public decimal AverageItemPrice { get; private set; }
+        public bool IsActive { get; private set; }
+
+
+        public Campaign(string name, string productCode, int duration, int priceManipulationLimit, int targetSalesCount)
+        {
+            Name = name;
+            ProductCode = productCode;
+            Duration = duration;
+            PriceManipulationLimit = priceManipulationLimit;
+            TargetSalesCount = targetSalesCount;
+            IsActive = true;
+        }
+
+        public void SetInActiveStatus()
+        {
+            IsActive = false;
+        }
+
+        public void SetTotalSalesCount(int quantity)
+        {
+            TotalSalesCount += quantity;
+        }
+
+        public void SetAverageItemPrice(decimal averageItemPrice)
+        {
+            AverageItemPrice = averageItemPrice;
+        }
+    }
+}
---
+++ tests/CampaignModule.UnitTests/Domain/CampaignAggregateTest.cs
@@ -0,0 +1,86 @@
+﻿using CampaignModule.Domain.AggregatesModel.CampaignAggregate;
+using Xunit;
+
+namespace CampaignModule.UnitTests.Domain
+{
+    public class CampaignAggregateTest
+    {
+        [Fact]
+        public void Create_campaign_success()
+        {
+            //Arrange    
+            var name = "FakeCampaignName";
+            var productCode = "FakeProductCode";
+            var duration = 10;
+            var priceManipulationLimit = 20;
+            var targetSalesCount = 100;
+
+
+            //Act 
+            var fakeCampaignItem = new Campaign(name, productCode, duration, priceManipulationLimit, targetSalesCount);
+
+            //Assert
+            Assert.NotNull(fakeCampaignItem);
+        }
+
+
+        [Fact]
+        public void Campaign_should_inactive_status_set()
+        {
+            //Arrange
+            var name = "FakeCampaignName";
+            var productCode = "FakeProductCode";
+            var duration = 10;
+            var priceManipulationLimit = 20;
+            var targetSalesCount = 100;
+            var fakeCampaignItem = new Campaign(name, productCode, duration, priceManipulationLimit, targetSalesCount);
+
+
+            //Act 
+            fakeCampaignItem.SetInActiveStatus();
+
+            //Assert
+            Assert.False(fakeCampaignItem.IsActive);
+        }
+
+        [Fact]
+        public void Campaign_should_total_sales_count_set()
+        {
+            //Arrange
+            var name = "FakeCampaignName";
+            var productCode = "FakeProductCode";
+            var duration = 10;
+            var priceManipulationLimit = 20;
+            var targetSalesCount = 100;
+            var fakeCampaignItem = new Campaign(name, productCode, duration, priceManipulationLimit, targetSalesCount);
+
+
+            //Act 
+            fakeCampaignItem.SetTotalSalesCount(5);
+
+            //Assert
+            Assert.Equal(5, fakeCampaignItem.TotalSalesCount);
+        }
+
+        [Fact]
+        public void Campaign_should_average_item_price_set()
+        {
+            //Arrange
+            var name = "FakeCampaignName";
+            var productCode = "FakeProductCode";
+            var duration = 10;
+            var priceManipulationLimit = 20;
+            var targetSalesCount = 100;
+            var fakeCampaignItem = new Campaign(name, productCode, duration, priceManipulationLimit, targetSalesCount);
+
+
+            //Act 
+            fakeCampaignItem.SetAverageItemPrice(5);
+
+            //Assert
+            Assert.Equal(5, fakeCampaignItem.AverageItemPrice);
+        }
+
+
+    }
+}
