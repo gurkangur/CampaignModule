5e5f5bdcc4d0b690cf775b17b8167aa471b23fce
gurkangur <gurkangur154@gmail.com>
2022-01-01 16:21:13 +0300

    add order entity

---
+++ src/CampaignModule/Domain/AggregatesModel/OrderAggregate/Order.cs
@@ -0,0 +1,26 @@
+﻿using CampaignModule.Domain.SeedWork;
+using System;
+
+namespace CampaignModule.Domain.AggregatesModel.OrderAggregate
+{
+    public class Order : Entity
+    {
+        public string ProductCode { get; private set; }
+        public int Quantity { get; private set; }
+        public decimal UnitPrice { get; private set; }
+
+        public Order(string productCode, int quantity)
+        {
+            if (quantity <= 0)
+            {
+                throw new Exception("Invalid number of quantity");
+            }
+            ProductCode = productCode;
+            Quantity = quantity;
+        }
+        public void SetPrice(decimal unitPrice)
+        {
+            UnitPrice = unitPrice;
+        }
+    }
+}
---
+++ tests/CampaignModule.UnitTests/Domain/OrderAggregateTest.cs
@@ -0,0 +1,44 @@
+﻿using CampaignModule.Domain.AggregatesModel.OrderAggregate;
+using System;
+using Xunit;
+
+namespace CampaignModule.UnitTests.Domain
+{
+    public class OrderAggregateTest
+    {
+        [Fact]
+        public void Create_order_success()
+        {
+            //Arrange    
+            var productCode = "FakeProductCode";
+            var quantity = 5;
+
+            //Act 
+            var fakeOrderItem = new Order(productCode, quantity);
+
+            //Assert
+            Assert.NotNull(fakeOrderItem);
+        }
+        [Fact]
+        public void Invalid_number_of_quantity()
+        {
+            //Arrange    
+            var productCode = "FakeProductCode";
+            var quantity = -1;
+
+            //Act - Assert
+            Assert.Throws<Exception>(() => new Order(productCode, quantity));
+        }
+        [Fact]
+        public void Order_should_price_set()
+        {
+            //Arrange
+            var order = new Order("FakeProductCode", 1);
+
+            //Act
+            order.SetPrice(50);
+            //expected
+            Assert.Equal(50, order.UnitPrice);
+        }
+    }
+}
