6a1581eb0c7cde888003a69abe7f53e77f84be4c
gurkangur <gurkangur154@gmail.com>
2022-01-01 16:16:36 +0300

    add product entity

---
+++ src/CampaignModule/Domain/AggregatesModel/ProductAggregate/ProductAggregate.cs
@@ -0,0 +1,33 @@
+﻿using CampaignModule.Domain.SeedWork;
+using System;
+
+namespace CampaignModule.Domain.AggregatesModel.ProductAggregate
+{
+    public class Product : Entity
+    {
+        public string Code { get; private set; }
+        public int Stock { get; private set; }
+        public decimal Price { get; private set; }
+        public decimal ListPrice { get; private set; }
+
+        public Product(string code, decimal price, int stock)
+        {
+            Code = code;
+            Price = price;
+            ListPrice = price;
+            Stock = stock;
+        }
+
+        public void SetSalePrice(decimal price)
+        {
+            Price = price;
+        }
+        public void DecreaseStock(int quantity)
+        {
+            if (Stock - quantity < 0)
+                throw new Exception("Out of stock");
+
+            Stock -= quantity;
+        }
+    }
+}
---
+++ tests/CampaignModule.UnitTests/Domain/ProductAggregateTest.cs
@@ -0,0 +1,65 @@
+﻿using CampaignModule.Domain.AggregatesModel.ProductAggregate;
+using System;
+using Xunit;
+
+namespace CampaignModule.UnitTests.Domain
+{
+    public class ProductAggregateTest
+    {
+        [Fact]
+        public void Create_product_success()
+        {
+            //Arrange    
+            var code = "FakeProductCode";
+            var price = 5;
+            var stock = 5;
+
+
+            //Act 
+            var fakeProductItem = new Product(code, price, stock);
+
+            //Assert
+            Assert.NotNull(fakeProductItem);
+        }
+        [Fact]
+        public void Product_Should_Price_Set()
+        {
+            //Arrange
+            var price = 5;
+            var product = new Product("FakeProductCode", price, 1);
+
+            //Act
+            var newPrice = 50;
+            product.SetSalePrice(newPrice);
+
+            //expected
+            Assert.Equal(newPrice, product.Price);
+            Assert.Equal(price, product.ListPrice);
+        }
+        [Fact]
+        public void Product_Should_Decrease_Stock()
+        {
+            //Arrange
+            var stock = 100;
+            var product = new Product("FakeProductCode", 5, stock);
+
+            //Act
+            var quantity = 50;
+            product.DecreaseStock(quantity);
+
+            //expected
+            Assert.Equal(stock - quantity, product.Stock);
+        }
+
+        [Fact]
+        public void DecreaseStock_Invalid_number_of_quantity()
+        {
+            //Arrange    
+            var stock = 100;
+            var product = new Product("FakeProductCode", 5, stock);
+
+            //Act - Assert
+            Assert.Throws<Exception>(() => product.DecreaseStock(101));
+        }
+    }
+}
